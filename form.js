03734a77603099e447a002368398ffe3e123e420
damilare dolapo jimoh <ezekieldammy1@gmail.com>
2023-11-07 12:20:44 +0100

    form page has been modified

--- form.js
+++ form.js
@@ -1,4 +1,10 @@
 // DOM Elements
+let lastNameInput;
+let firstNameInput;
+let userNameInput;
+let passwordInput;
+let emailInput;
+
 const firstName = document.querySelector(".first-name");
 const lastName = document.querySelector(".last-name");
 const signIn = document.querySelector(".submit");
@@ -9,24 +15,28 @@ const showPassword = document.querySelector(".eye-open");
 const confirmPassword = document.querySelector(".confirm-pass");
 const passwordError = document.querySelector(".password-error");
 const confirmPasswordError = document.querySelector(".confirm-pass-error");
-
+const emailAddress = document.querySelector(".email");
 
 // Submit Button Event Listener
 signIn.addEventListener("click", () => {
   validateNames();
   validateUserName();
   validatePasswords();
+  
 });
 
 // Function to Validate First Name and Last Name, Converting Them to Uppercase
 const validateNames = () => {
+  firstNameInput = firstName.value;
+  lastNameInput = lastName.value;
+
   // Regular expression to check if all characters in first and last name are uppercase
   const uppercaseRegex = /[A-Z]/g;
 
   // Check if first and last name are all uppercase letters
   if (
-    !uppercaseRegex.test(firstName.value) ||
-    !uppercaseRegex.test(lastName.value)
+    !uppercaseRegex.test(firstNameInput) ||
+    !uppercaseRegex.test(lastNameInput)
   ) {
     alert("First Name and Last Name Must be in Uppercase");
   }
@@ -35,7 +45,7 @@ const validateNames = () => {
 // Function to Validate User Name and Prevent Form Submission if It's Incorrect
 const validateUserName = () => {
   // Get the input from the user name field
-  const userNameInput = userName.value;
+  userNameInput = userName.value;
 
   // Regular expression for validating username
   const usernameRegex = /^(?=.*[a-zA-Z])(?=.*\d).+$/;
@@ -43,37 +53,49 @@ const validateUserName = () => {
   // Check if the username is correct
   if (usernameRegex.test(userNameInput)) {
     userNameError.style.display = "none"; // Hide the error message
+    userName.value = "";
   } else {
-    userNameInput = userName.value;
+    // userNameInput.value;
     userNameError.textContent =
-      "Username must include both letters and numbers enter the firstname and lastname again";
+      "Username must include both letters and numbers";
+    alert(`ERROR`)
+  }
+
+  emailInput = emailAddress.value;
+  if (emailInput) {
+    emailAddress.value = "";
   }
 };
 // Function to Validate Password
 const validatePasswords = () => {
+  passwordInput = password.value;
   // Regular expression to validate the password pattern
   const validPasswordRegex =
     /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
 
   // Check if the password passes the test
-  if (validPasswordRegex.test(password.value)) {
+  if (validPasswordRegex.test(passwordInput)) {
     passwordError.textContent = ""; // Clear the error message
     // Check if password and confirm password match
-    if (password.value === confirmPassword.value) {
+    if (passwordInput === confirmPassword.value) {
       // Clear input fields and error messages
       password.value = "";
       confirmPassword.value = "";
       firstName.value = "";
       lastName.value = "";
-      userName.value = "";
-      confirmPasswordError.textContent = "";
-    } else {
-      confirmPasswordError.textContent = "Please enter the correct password";
+      passwordInput = "";
+      // confirmPasswordError.textContent = "";
     }
   } else {
     passwordError.textContent =
       "Password must contain at least a symbol, letter, and number";
   }
+  alert(`Please confirm your details: 
+  First Name: ${firstNameInput}
+  Last Name : ${lastNameInput}
+  User Name: ${userNameInput}
+  Email Address: ${emailInput}
+  Password : ***********`);
 };
 
 // Function to Toggle Password Visibility
